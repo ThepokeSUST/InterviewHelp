0b604d246988877838b7f804a3e0891359da294d
dipok <dipoknathdipu@gmail.com>
2026-07-06 23:48:07 +0600

    fix cors problem

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,32 @@
+// const express = require('express');
+// const app = express();
+// const CookieParser = require("cookie-parser");
+// const cors= require('cors')
+
+// //require all routes
+// const authRoute= require("./routes/auth.route")
+// const interviewRoute= require("./routes/interview.route")
+
+// app.use(express.json());
+// app.use(CookieParser());
+// app.use(express.urlencoded({extended:true}));
+// app.set('trust proxy', 1);
+// const FRONTEND_URL = process.env.FRONTEND_URL; 
+// app.use(cors({
+//     origin: 'https://interviewhelp-1-52zj.onrender.com',
+//     credentials: true
+// }));
+
+
+
+// app.use('/api/auth',authRoute);
+// app.use('/api/interview',interviewRoute);
+
+// module.exports = app;
+
+
+
+
 const express = require('express');
 const app = express();
 const CookieParser = require("cookie-parser");
@@ -7,15 +36,15 @@ const cors= require('cors')
 const authRoute= require("./routes/auth.route")
 const interviewRoute= require("./routes/interview.route")
 
-app.use(express.json());
-app.use(CookieParser());
-app.use(express.urlencoded({extended:true}));
 app.set('trust proxy', 1);
 const FRONTEND_URL = process.env.FRONTEND_URL; 
 app.use(cors({
     origin: 'https://interviewhelp-1-52zj.onrender.com',
     credentials: true
 }));
+app.use(express.json());
+app.use(CookieParser());
+app.use(express.urlencoded({extended:true}));
 
 
 
